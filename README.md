c28eb16bfc2ed0649b9f267ac8eec3345527a18c
Joe Kent <thedeadlybutter@gmail.com>
2016-10-10 22:31:59 -0400

    Updates server to allow for custom events

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup
+- Install [Git](https://help.github.com/desktop/guides/getting-started/installing-github-desktop/) if you don't already have it
+- `git clone https://github.com/deadlybutter/dig.git`
+- [Install NodeJS & NPM](https://docs.npmjs.com/getting-started/installing-node)
+- [Install Heroku Toolbelt](https://devcenter.heroku.com/articles/heroku-command-line#download-and-install)
+- Run `npm install` inside the dig_server folder
+- Run `npm start` to launch the server
+
+Should be accessible at `localhost:5000`.
--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, function () {
+const server = app.listen(process.env.PORT, function () {
   console.log(`Dig server running on ${process.env.PORT}`);
+});
+
+const io = require('socket.io')(server);
+const events = ['CLIENT_JOIN', 'MAP_RECIEVE', 'ENTITY_LOCATION_CHANGE', 'ENTITY_ADD'];
+
+io.on('connection', function (socket) {
+  socket.join('testroom');
+  socket.emit('SOCKET_ID', {socketId: socket.id, roomId: 'testroom'});
+
+  events.forEach(eventName => {
+    socket.on(eventName, data => {
+      const destination = data._headers.to === 'all' ? data._headers.room : data._headers.to;
+      socket.broadcast.to(destination).emit(eventName, data);
+    });
+  });
 });
